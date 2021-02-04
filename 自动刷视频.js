5bfe3e6a64ae278353e9fda0a9673823a3eef56a
mpcjk-doctor <54452356+mpcjk-doctor@users.noreply.github.com>
2021-02-04 13:17:27 +0800

    Add files via upload

---
+++ 自动刷视频.js
@@ -0,0 +1,604 @@
+/* 
+ * 该脚本整合了快手极速版//抖音极速版//火山极速版//刷宝短视频//彩蛋视频...(后续还可能更新其他)刷视频功能
+ * 加上快手极速版每日任务*/
+
+let kuaishou = {//快手极速版
+    run: function (runTimes) {
+        toast('这是一个快手刷视频脚本,1s之后打开APP');
+        //toast:弹窗函数
+        sleep(1000);
+        //sleep以ms计算
+        var launchResult = app.launchApp("快手极速版");
+        
+        if (!launchResult) {
+            toast('你还没有安装快手极速版！');
+            sleep(1000)
+            return;
+        }
+        toast('等待软件打开，3s之后进入下个动作！');
+        sleep(3000);
+        var sleepTime = 10;
+        // 统计运行次数
+        var flagTime = 0;
+        while (true) {
+            flagTime++;
+            // 超过次数终止程序
+            if (flagTime > runTimes) {
+                break;
+            }
+            sleepTime = randNum(5, 15);
+            toast(Math.floor(sleepTime).toString() + 's之后跳到下个视频！已经执行' + flagTime.toString() + "次");
+            sleep(sleepTime * 1000);
+            nextVideo();
+        }
+        home();
+        sleep(1000);
+
+        function nextVideo() {/*视频下滑功能*/
+            //获得手机分辨率
+            var width = device.width;
+            var height = device.height;
+            swipe(width / 2, height / 2, width / 2, height / 3, 10);
+            
+            var j = Math.floor(random(1,5))//随意调参
+            if (j == 2){//随机回看功能
+
+                toast("开始往回看一个视频");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/4),device.width/2,device.height*(3/4),500);
+                
+                toast("休息随机时间") ;
+                sleep(3000);
+
+                sleep(random(10000,15000));
+                  
+            
+
+
+                toast("开始往下看两个视频1");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/2),device.width/2,device.height*(1/4),10);
+                
+                toast("休息随机时间1");
+                sleep(3000);
+                
+                sleep(random(1000,5001));
+                
+
+                toast("开始往下看两个视频2");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/2),device.width/2,device.height*(1/4),10);
+                
+                toast("休息随机时间2");
+                sleep(3000);
+                
+                sleep(random(1000,10001));
+                
+                
+   
+            }
+        }
+
+        function randNum(minnum, maxnum) {//获取范围内的随机数
+            return minnum + Math.random() * (maxnum - minnum);
+        };
+        
+
+    
+    }
+};
+
+let douyin = {//抖音极速版
+    run: function (runTimes) {
+        toast('这是一个抖音刷视频脚本,1s之后打开APP');
+        //toast:弹窗函数
+        sleep(1000);
+        //sleep以ms计算
+        var launchResult = app.launchApp("抖音极速版");
+        
+        if (!launchResult) {
+            toast('你还没有安装抖音极速版！');
+            sleep(1000)
+            return;
+        }
+        toast('等待软件打开，3s之后进入下个动作！');
+        sleep(3000);
+        var sleepTime = 10;
+        // 统计运行次数
+        var flagTime = 0;
+        while (true) {
+            flagTime++;
+            // 超过次数终止程序
+            if (flagTime > runTimes) {
+                break;
+            }
+            sleepTime = randNum(5, 15);
+            toast(Math.floor(sleepTime).toString() + 's之后跳到下个视频！已经执行' + flagTime.toString() + "次");
+            sleep(sleepTime * 1000);
+            nextVideo();
+        }
+        home();
+        sleep(1000);
+
+        function nextVideo() {/*视频下滑功能*/
+            //获得手机分辨率
+            var width = device.width;
+            var height = device.height;
+            swipe(width / 2, height / 2, width / 2, height / 3, 10);
+            
+            var j = Math.floor(random(1,5))//随意调参
+            if (j == 2){//随机回看功能
+
+                toast("开始往回看一个视频");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/4),device.width/2,device.height*(3/4),500);
+                
+                toast("休息随机时间") ;
+                sleep(3000);
+
+                sleep(random(10000,15000));
+                  
+            
+
+
+                toast("开始往下看两个视频1");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/2),device.width/2,device.height*(1/4),10);
+                
+                toast("休息随机时间1");
+                sleep(3000);
+                
+                sleep(random(1000,5001));
+                
+
+                toast("开始往下看两个视频2");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/2),device.width/2,device.height*(1/4),10);
+                
+                toast("休息随机时间2");
+                sleep(3000);
+                
+                sleep(random(1000,10001));
+                
+                
+   
+            }
+        }
+
+        function randNum(minnum, maxnum) {//获取范围内的随机数
+            return minnum + Math.random() * (maxnum - minnum);
+        };
+        
+
+    
+    }
+};
+
+let huoshan = {//火山极速版
+    run: function (runTimes) {
+        toast('这是一个火山极速版刷视频脚本,1s之后打开APP');
+        //toast:弹窗函数
+        sleep(1000);
+        //sleep以ms计算
+        var launchResult = app.launchApp("火山极速版");
+        
+        if (!launchResult) {
+            toast('你还没有安装火山极速版！');
+            sleep(1000)
+            return;
+        }
+        toast('等待软件打开，3s之后进入下个动作！');
+        sleep(3000);
+        var sleepTime = 10;
+        // 统计运行次数
+        var flagTime = 0;
+        while (true) {
+            flagTime++;
+            // 超过次数终止程序
+            if (flagTime > runTimes) {
+                break;
+            }
+            sleepTime = randNum(5, 15);
+            toast(Math.floor(sleepTime).toString() + 's之后跳到下个视频！已经执行' + flagTime.toString() + "次");
+            sleep(sleepTime * 1000);
+            nextVideo();
+        }
+        home();
+        sleep(1000);
+
+        function nextVideo() {/*视频下滑功能*/
+            //获得手机分辨率
+            var width = device.width;
+            var height = device.height;
+            swipe(width / 2, height / 2, width / 2, height / 3, 10);
+            
+            var j = Math.floor(random(1,5))//随意调参
+            if (j == 2){//随机回看功能
+
+                toast("开始往回看一个视频");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/4),device.width/2,device.height*(3/4),500);
+                
+                toast("休息随机时间") ;
+                sleep(3000);
+
+                sleep(random(10000,15000));
+                  
+            
+
+
+                toast("开始往下看两个视频1");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/2),device.width/2,device.height*(1/4),10);
+                
+                toast("休息随机时间1");
+                sleep(3000);
+                
+                sleep(random(1000,5001));
+                
+
+                toast("开始往下看两个视频2");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/2),device.width/2,device.height*(1/4),10);
+                
+                toast("休息随机时间2");
+                sleep(3000);
+                
+                sleep(random(1000,10001));
+                
+                
+   
+            }
+        }
+
+        function randNum(minnum, maxnum) {//获取范围内的随机数
+            return minnum + Math.random() * (maxnum - minnum);
+        };
+        
+
+    
+    }
+};
+
+let shuabao = {//刷宝短视频
+    run: function (runTimes) {
+        toast('这是一个刷宝刷视频脚本,1s之后打开APP');
+        //toast:弹窗函数
+        sleep(1000);
+        //sleep以ms计算
+        var launchResult = app.launchApp("刷宝短视频");
+        //app.launchApp("com.kuaishou.nebula");
+        if (!launchResult) {
+            toast('你还没有安装刷宝短视频！');
+            sleep(1000)
+            return;
+        }
+        toast('等待软件打开，3s之后进入下个动作！');
+        sleep(3000);
+        var sleepTime = 10;
+        // 统计运行次数
+        var flagTime = 0;
+        while (true) {
+            flagTime++;
+            // 超过次数终止程序
+            if (flagTime > runTimes) {
+                break;
+            }
+            sleepTime = randNum(5, 15);
+            toast(Math.floor(sleepTime).toString() + 's之后跳到下个视频！已经执行' + flagTime.toString() + "次");
+            sleep(sleepTime * 1000);
+            nextVideo();
+        }
+        home();
+        sleep(1000);
+
+        function nextVideo() {/*视频下滑功能*/
+            //获得手机分辨率
+            var width = device.width;
+            var height = device.height;
+            swipe(width / 2, height / 2, width / 2, height / 3, 10);
+            
+            var j = Math.floor(random(1,5))//随意调参
+            if (j == 2){//随机回看功能
+
+                toast("开始往回看一个视频");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/4),device.width/2,device.height*(3/4),500);
+                
+                toast("休息随机时间") ;
+                sleep(3000);
+
+                sleep(random(10000,15000));
+                  
+            
+
+
+                toast("开始往下看两个视频1");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/2),device.width/2,device.height*(1/4),10);
+                
+                toast("休息随机时间1");
+                sleep(3000);
+                
+                sleep(random(1000,5001));
+                
+
+                toast("开始往下看两个视频2");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/2),device.width/2,device.height*(1/4),10);
+                
+                toast("休息随机时间2");
+                sleep(3000);
+                
+                sleep(random(1000,10001));
+                
+                
+   
+            }
+        }
+
+        function randNum(minnum, maxnum) {//获取范围内的随机数
+            return minnum + Math.random() * (maxnum - minnum);
+        };
+        
+
+    
+    }
+};
+
+let caidan = {//彩蛋视频
+    run: function (runTimes) {
+        toast('这是一个彩蛋刷视频脚本,1s之后打开APP');
+        //toast:弹窗函数
+        sleep(1000);
+        //sleep以ms计算
+        var launchResult = app.launchApp("彩蛋视频");
+        //app.launchApp("com.kuaishou.nebula");
+        if (!launchResult) {
+            toast('你还没有安装彩蛋视频！');
+            sleep(1000)
+            return;
+        }
+        toast('等待软件打开，3s之后进入下个动作！');
+        sleep(3000);
+        var sleepTime = 10;
+        // 统计运行次数
+        var flagTime = 0;
+        while (true) {
+            flagTime++;
+            // 超过次数终止程序
+            if (flagTime > runTimes) {
+                break;
+            }
+            sleepTime = randNum(5, 15);
+            toast(Math.floor(sleepTime).toString() + 's之后跳到下个视频！已经执行' + flagTime.toString() + "次");
+            sleep(sleepTime * 1000);
+            // if (id("btn_layout").exists()) {
+
+            //     toast("进入翻倍中");
+
+            //     sleep(1000);
+            //     console.log(id("btn_layout").untilFind().longClick())
+            //     sleep(1000);
+            //     while (true) {
+            //         toast("检测中");
+            //         sleep(1000);
+            //         if ( className("android.widget.ImageView").depth(5).exists()) {
+            //             console.log("退出:"+className("android.widget.ImageView").depth(5).untilFind().longClick());
+            //             // className("android.widget.ImageView").depth(5).findOne().click()
+            //             sleep(1000)
+            //             break;
+            //         }
+            //     }
+            // }
+            // else   
+            if (className("android.widget.TextView").text("立即领取").exists()) {
+                toast("播放广告中");
+                while (className("android.widget.TextView").text("立即领取").exists()) {
+                    toast("等待中……");
+                    click(1000, 1000);//点击部位?
+                    sleep(1000)
+                }
+            }
+            nextVideo();
+        }
+        home();
+        sleep(1000);
+
+        function nextVideo() {/*视频下滑功能*/
+            //获得手机分辨率
+            var width = device.width;
+            var height = device.height;
+            swipe(width / 2, height / 2, width / 2, height / 3, 10);
+            
+            var j = Math.floor(random(1,5))//随意调参
+            if (j == 2){//随机回看功能
+
+                toast("开始往回看一个视频");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/4),device.width/2,device.height*(3/4),500);
+                
+                toast("休息随机时间") ;
+                sleep(3000);
+
+                sleep(random(10000,15000));
+                  
+            
+
+
+                toast("开始往下看两个视频1");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/2),device.width/2,device.height*(1/4),10);
+                
+                toast("休息随机时间1");
+                sleep(3000);
+                
+                sleep(random(1000,5001));
+                
+
+                toast("开始往下看两个视频2");
+                sleep(3000);
+
+                swipe(device.width/2, device.height*(1/2),device.width/2,device.height*(1/4),10);
+                
+                toast("休息随机时间2");
+                sleep(3000);
+                
+                sleep(random(1000,10001));
+                
+                
+   
+            }
+        }
+
+        function randNum(minnum, maxnum) {//获取范围内的随机数
+            return minnum + Math.random() * (maxnum - minnum);
+        };
+        
+
+    
+    }
+};
+
+
+
+function main(i) {/*主函数*/
+    home();
+    sleep(3000);
+
+    while (i > 0) {
+        kuaishou.run(Math.floor(random(20,30)))//参数为每次循环刷动的次数
+        shuabao.run(Math.floor(random(20,30)))//参数为每次循环刷动的次数
+        huoshan.run(Math.floor(random(20,30)))//参数为每次循环刷动的次数
+        caidan.run(Math.floor(random(20,30)))//参数为每次循环刷动的次数
+        douyin.run(Math.floor(random(20,30)))//参数为每次循环刷动的次数
+        i--;
+    }
+        // qkk.open()//打开趣看看短视频
+        // .article(50, 10)//阅读文章数，每篇文章滑动次数
+        // .video(1000)//看的短视频数量
+}
+
+
+
+auto.waitFor()
+/**
+ * 快手极速版每日任务程序部分
+ */
+/**-------------------------打开APP-------------------------------- */
+//let times = rawInput("请输入要自动刷的视频次数：", "500")  //用户设置刷视频的个数，默认100
+launchApp("快手极速版");
+console.show()  //显示悬浮窗（需要先打开悬浮窗权限）
+sleep(5000)    //等待应用打开
+console.log("准备就绪！")
+
+dytimes(1000, 3000)
+/**-------------------------打开福利页面-------------------------------- */
+id("left_btn").click()//点击菜单
+dytimes(2000, 4000)
+drawingOrder("3").id("container").click()// drawingOrder("2") 点击去赚钱
+dytimes(2000, 4000)
+/**-------------------------调用方法-------------------------------- */
+welfare()
+dytimes(1000, 3000)
+liveds()
+dytimes(1000, 3000)
+
+console.hide()
+home();//回到首页
+/**-------------------------10次福利金币-------------------------------- */
+function welfare() {
+    swipe(device.width / 2, device.height * (8 / 9), device.width / 2, device.height * (1 / 2), 150)
+    dytimes(3000, 5000);
+    var i = 1
+    while (true) {
+        i = i + 1;
+        if (text("福利 领金币").findOnce()) {
+            text("福利 领金币").click();
+            console.log("第" + i + "次广告福利")
+            //等待广告关闭按钮点击
+            dytimes(3000, 5000);
+            id("video_close_icon").waitFor()
+            id("video_close_icon").click()
+            dytimes(3000, 5000);
+        }
+        if (text("福利").findOnce()) {
+            text("福利").click();
+            console.log("第" + i + "次广告福利")
+            //等待广告关闭按钮点击
+            dytimes(3000, 5000);
+            id("video_close_icon").waitFor()
+            id("video_close_icon").click()
+            dytimes(3000, 5000);
+        }
+        if (className("android.view.View").text("明天看视频继续领取1000金币").exists() || i == 20) {
+            console.log("10次广告福利结束啦！" + i);
+            break;
+        }
+    }
+}
+/**-------------------------10次直播金币-------------------------------- */
+function liveds() {
+    swipe(device.width / 2, device.height * (8 / 9), device.width / 2, device.height * (1 / 4), 150)
+    dytimes(3000, 5000);
+    console.log("开始看直播10次咯！")
+    if (className("android.view.View").text("今日已成功领取直播奖励金币").exists()) {
+        console.log("10次直播福利结束啦！二次运行");
+        back()
+    }
+    else {
+        className("android.view.View").text("直播送礼赚金币").findOne().parent().click()
+        text("看直播").click()
+        for (let i = 1; i <= 10; i++) {
+            console.log("第" + i + "次直播奖励")
+            sleep(32 * 1000)
+            swipe(device.width / 2, device.height * (8 / 9), device.width / 2, device.height * (1 / 4), 150)
+            if (text("close_btn").exists()) {
+                text("close_btn").click()
+                console.log("有关注主播弹出");
+                // id("close_btn").waitFor()
+            } else if (i == 10) {
+                console.log("10次直播福利结束啦！里面");
+                console.hide()
+                home();//回到首页
+                // back();
+                // back();
+                // back();
+                // if (id("exit_btn").exists()) {
+                //     id("exit_btn").findOne().click()
+                //     back();
+                //     sleep(3000)
+                //     back();
+                // }
+
+                
+            }
+        }
+    }
+}
+/**-------------------------随机时间-------------------------------- */
+function dytimes(time1, time2) {
+    delayTime = random(time1, time2)
+    sleep(delayTime)    //在视频停留8-12秒
+}
+/** 快手极速版每日任务程序部分结束 */
+
+
+
+
+var times = rawInput("请输入要自动刷的视频次数：","30"); 
+main(times);
